cabc77b02b88426ce8d1b1f5f4a7cf1f37b80bda
lauracb <lauracadavidb@gmail.com>
2019-03-19 18:46:15 -0500

    Modificando arrays usando estados

--- src/App.js
+++ src/App.js
@@ -2,10 +2,58 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      tasks: ["Tarea 1", "Tarea 2"]
+    }
+  }
+
+
   render() {
     return (
-      <h1>Hola Mundo</h1>
+      <div>
+        <h1>Hola Mundo</h1>
+        <ul>
+          {this.state.tasks.map(task =>
+            <li>{task}</li>
+          )}
+        </ul>
+        <button onClick={this.addTask.bind(this)}>Agregue un elemento</button>
+        <button onClick={this.updateTask.bind(this)}>Modificar tarea 2</button>
+      </div>
     );
+  };
+
+  addTask() {
+    this.setState({
+      tasks: this.state.tasks.concat("Nuevo elemento")
+    });
+  }
+
+  updateTask() {
+    const index = this.state.tasks.findIndex(task =>
+        task === "Tarea 2"
+      );
+
+  // this.setState({
+  //   tasks: this.state.tasks.map((task, i) =>
+  //     i === index ? "Actualizado" : task
+  //   )
+  // })
+
+  const tasks = this.state.tasks;
+  
+  this.setState({
+    tasks: [
+      ...tasks.slice(0, index),
+      "Actualizado",
+      ...tasks.slice(index + 1)
+    ]
+  });
+
+
   }
 }
 
