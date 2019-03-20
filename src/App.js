46dd28a3f173603d6c0a4f050fc09d5ef5e42137
lauracb <lauracadavidb@gmail.com>
2019-03-19 19:01:33 -0500

    Formulario con React

--- src/App.js
+++ src/App.js
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name : "",
+      terms: false
+    }
+  }
+
+
   render() {
     return (
-      <h1>Hola Mundo</h1>
+      <div>
+        <h1>Hola Mundo</h1>
+        <input type="text"  value={this.state.name} onChange={this.updateName.bind(this)}/>
+        <div>
+          <label>
+            <input type="checkbox" checked={this.state.terms} onClick={this.updateTerms.bind(this)} /> Acepto los términos.
+          </label>
+        </div>
+        <button onClick={this.sayHi.bind(this)}>Say Hi!</button>
+      </div>
     );
   }
+
+  updateTerms(event){
+    this.setState({
+      terms: event.target.checked
+    });
+  }
+
+  updateName(event){
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  sayHi(){
+    if(this.state.terms){
+      alert('Hola ' + this.state.name);
+    } else {
+      alert('Debes aceptar los términos')
+    }
+  } 
 }
 
 export default App;
