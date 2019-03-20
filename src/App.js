00db1a4618decf7b25489de98ebed042dcdda7e1
lauracb <lauracadavidb@gmail.com>
2019-03-19 19:23:42 -0500

    Peticion con axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      names: []
+    };
+
+    axios.get("http://localhost:3001/names")
+    .then(response => {
+      console.log(response);
+      this.setState({
+        names: response.data
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    })
+
+  }
+
   render() {
     return (
-      <h1>Hola Mundo</h1>
+      <div>
+        <h1>Hola Mundo</h1>
+        <ul>
+          {this.state.names.map(name => 
+            <li key={name}>{name}</li>  
+          )}
+        </ul>
+      </div>
     );
   }
 }
