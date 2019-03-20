34371823558512166b1e643f1e9fe7373913cf63
lauracb <lauracadavidb@gmail.com>
2019-03-19 22:01:54 -0500

    Rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import { Router, Route, browserHistory, Link, IndexRoute } from 'react-router'
 import './App.css';
+
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const NoMatch = () => <h1>Página no encontrada</h1>
+const Message = (props) => <h1> Mensaje {props.params.id}</h1>
+
+const Template = () => {
+  return(
+    <div>
+      <h1>Mi super aplicación</h1>
+      {props.children}
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="about">About</Link></li>
+      </ul>
+    </div>
+  )
+}
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hola Mundo</h1>
+      <Router history={browserHistory}>
+        <Route path="/" component={Template} >
+          <IndexRoute component={Home} />
+          <Route path="about" component={About} />
+        </Route >
+        <Route path="*" component={NoMatch} />
+        <Route path="messages/:id" component={Message} />
+      </Router>
     );
   }
 }
