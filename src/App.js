af9ee22dd3d4dedc7ac6072f5f1feaae4e380d0f
lauracb <lauracadavidb@gmail.com>
2019-03-19 20:51:53 -0500

    Botón bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hola Mundo</h1>
+      <div>
+        <h1>Hola Mundo</h1>
+        <Button variant="primary">Primary</Button>
+      </div>
     );
   }
 }
